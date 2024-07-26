35bcad22eef381772b2b4f104c263277229778b4
boykin345 <juzzyboykin@gmail.com>
2024-07-26 22:28:29 +0300

    ligature test

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("FreakyBob");
+        System.out.println("FreakyBob\n");
+        int b = 3;
+        if (b <= 4) {
+            System.out.println("not real");
+        }
     }
 }
