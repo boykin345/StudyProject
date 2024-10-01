433104b3c91a0ef989a0bd033f9ed50d333a87da
boykin345 <juzzyboykin@gmail.com>
2024-10-01 17:22:02 +0100

    Add map functionality to Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<Integer, Integer> map = new java.util.HashMap<>();
+        map.put(1, map.getOrDefault(1, 0) + 1);
         System.out.println("FreakyBob\n");
         int b = 3;
         if (b <= 4) {
